aa8b92e4112e29c3c7db85837d218d83a418aa11
ybh <1041873882@qq.com>
2020-09-11 18:18:22 +0800

    第三次推送 2020/09/11

--- cgi/jni/obj/time.cpp
+++ cgi/jni/obj/time.cpp
@@ -15,7 +15,9 @@ void process(char **getvars, char **postvars, int form_method)
     req.request("/ui/web/time/read", NULL);
     p.load(req.body());
 
+
     if(form_method == POST) {
+
         char hour[12];
         int ntp;
         ntp = eGetInt("ntp");
@@ -24,10 +26,10 @@ void process(char **getvars, char **postvars, int form_method)
         if (!ntp) {
             strcpy(hour, eGetText("hour"));
             if (hour != NULL && atoi(hour) > 0 && atoi(hour) < 24) {
-                p.setText("/params/settime/hours", hour);
-            }
-        }        
-        req.request("/ui/web/time/write", p.data());
+                p.setText("/params/settime/hour", hour);
+            } 
+        } 
+        req.request("/ui/web/time/write", p.data());       
     }
 printf("    \n");
 printf("<html>\n");
@@ -38,8 +40,19 @@ printf("<link href=\"/css/style.css\" rel=\"stylesheet\" type=\"text/css\">\n");
 printf("\n");
 printf("</head>\n");
 printf("\n");
-printf("<body>\n");
+printf("<script>\n");
+printf("    function on_sync_button()\n");
+printf("    {\n");
+printf("        var d = new Date();\n");
+printf("        var h = String(d.getHours());\n");
+printf("        var m = String(d.getMinutes());\n");
+printf("        var s = String(d.getSeconds());\n");
+printf("        document.getElementById(\"text\").value = h+\":\"+m+\":\"+s;\n");
+printf("    }\n");
+printf("</script>\n");
 printf("\n");
+printf("\n");
+printf("<body>\n");
 printf("<form method=\"post\" action=\"time.cgi\">\n");
 printf("    <table width=\"90%%\" border=\"0\" id=\"table1\">\n");
 printf("        <tr>\n");
@@ -66,8 +79,11 @@ printf("                <tr>\n");
 printf("                    <td width=\"96\">");
  ePrint(ehttp_xml.get("/dnake/time/hour")); printf("</td>\n");
 printf("                    <td>\n");
-printf("                    <input style=\"width:80px;\" name=\"hour\" value=\"");
- ePrint(p.getText("/params/settime/hours")); printf("\"></td>\n");
+printf("                    <input  type=\"text\" style=\"width:160px;\" name=\"hour\" value=\"");
+ ePrint(p.getText("/params/settime/hour")); printf("\", id=\"text\"></td>\n");
+printf("                    <td>\n");
+printf("                    <input type=\"button\" value=\"");
+ ePrint(ehttp_xml.get("/dnake/sync_time")); printf("\" onClick=\"on_sync_button()\"></td>\n");
 printf("                </tr>\n");
 printf("                <tr>\n");
 printf("                    <td>&nbsp;</td>\n");
